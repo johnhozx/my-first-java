3bc1f697e088e14b9b5706adb9b8860bee14c96f
johnhozx <johnhozx@gmail.com>
2020-11-08 11:05:36 -0800

    my first commit

---
+++ Welcome1.java
@@ -0,0 +1,23 @@
+// My file name is Welcome1.java
+
+/*
+    This is the test
+*/
+
+public class Welcome1 {
+    public static void main(String[] args) {
+        // This line will print "welcome to java programming"
+        printMyName();
+        System.out.println("Welcome to Java Programming!");
+    }
+
+    // This method/function will print my name.
+    public static void printMyName() {
+        System.out.println("My name is Meron.");
+        printMyLastName();
+    }
+
+    public static void printMyLastName() {
+
+    }
+}
